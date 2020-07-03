7d3d5a63791d1ead8de6932a32d028bfd7db5c1f
mingyong.g <mingyong.g@foxmail.com>
2020-07-03 16:19:08 +0800

    修改publichPath

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: mingyong.g
+ * @Date: 2020-07-03 12:49:15
+ * @LastEditors: mingyong.g
+ * @LastEditTime: 2020-07-03 12:51:35
+ * @Description: 配置文件
+ * @FilePath: \my_foods_waterfull\vue.config.js
+ */
+module.exports = {
+  publicPath: "/my_waterfall_infinite/"
+};
