ad529ae6c4037bb9290fbaa792a82425577383c9
mingyong.g <mingyong.g@foxmail.com>
2020-07-03 22:49:52 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@
  * @Author: mingyong.g
  * @Date: 2020-07-03 22:45:49
  * @LastEditors: mingyong.g
- * @LastEditTime: 2020-07-03 22:47:46
+ * @LastEditTime: 2020-07-03 22:49:24
  * @Description: Vue配置文件
  * @FilePath: \my_foods_waterfull\vue.config.js
  */
 module.exports = {
-  publicgPath: "/my_foods_waterfull/"
+  publicPath: "/my_foods_waterfull/"
 };
